cb82c964f64bd3d97afff0301992077929668437
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-05-31 21:32:46 +0530

    video-29: How to make POST request with JSON data in golang

--- 21webrequests/main.go
+++ 21webrequests/main.go
@@ -13,9 +13,13 @@ func main() {
 	fmt.Println("Welcome to WebRequest in Go")
 
 	PerformGetRequest()
+	PerformCorrectPostJsonRequest()
+	PerformInCorrectPostJsonRequest()
 }
 
 func PerformGetRequest() {
+	fmt.Println("Handling get requests")
+
 	const myUrl = myHost + "/get"
 
 	response, err := http.Get(myUrl)
@@ -42,4 +46,56 @@ func PerformGetRequest() {
 	fmt.Println(string(content))
 	//another way
 	fmt.Println(resStr.String())
+}
+
+func PerformCorrectPostJsonRequest() {
+	fmt.Println("Handling Post requests")
+
+	const myUrl = myHost + "/post"
+
+	correctRequestBody := strings.NewReader(`
+		{
+			"courseName": "Go Tutorial",
+			"price": 69420,
+			"platform": "localhost:6942"
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", correctRequestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+}
+
+func PerformInCorrectPostJsonRequest() {
+	fmt.Println("Handling Incorrect Post requests")
+
+	const myUrl = myHost + "/post"
+
+	correctRequestBody := strings.NewReader(`
+		{
+			"courseName": "Go Tutorial",
+			"price": 69420
+			"platform": "localhost:6942"
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", correctRequestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
 }
