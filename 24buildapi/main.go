ad8ba6d11528f84fa88518c4355b9ed545389221
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-06-12 21:08:29 +0530

    video-36: Get one course based on request id in golang

--- 24buildapi/main.go
+++ 24buildapi/main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Model for course - file
@@ -43,4 +45,25 @@ func getAllCourses(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Get all courses")
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from request
+	params := mux.Vars(r)
+
+	fmt.Println("Params:", params)
+
+	// loop through the courses, find the matching id sent and then return the reponse
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No course found with the given courseId:" + params["id"])
+	return
 }
