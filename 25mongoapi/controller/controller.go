2ca65a33209729f1cbbe620c444e1a1ed78df736
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-06-16 21:17:07 +0530

    video-49: Mark movie as watched in golang

--- 25mongoapi/controller/controller.go
+++ 25mongoapi/controller/controller.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/djay-S/mongoapi/model"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -134,4 +135,24 @@ func GetAllMovies(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	allMovies := fetchAllMovies()
 	json.NewEncoder(w).Encode(allMovies)
+}
+
+func AddMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	var movie model.Netflix
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+
+	insertOneMovie(movie)
+	json.NewEncoder(w).Encode(movie)
+}
+
+func MarkMovieAsWatched(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Allow-Control-Allow-Methods", "POST")
+
+	params := mux.Vars(r)
+	updateOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
 }
