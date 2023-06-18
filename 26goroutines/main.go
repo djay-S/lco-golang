15d702ec8c0dce5d2b914b8b3a0db94113abe415
djay-S <dhananjaysinghkushwaha2@gmail.com>
2023-06-18 13:11:20 +0530

    video-53: Wait groups in golang

--- 26goroutines/main.go
+++ 26goroutines/main.go
@@ -2,17 +2,65 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"net/http"
+	"sync"
 )
 
+var waitGroup sync.WaitGroup // these are pointers
+
 func main() {
-	go greeter("Hello")
-	greeter("World")
+	// go greeter("Hello")
+	// greeter("World")
+
+	websiteList := []string{
+		"https://google.com",
+		"https://focalpost.com",
+		"https://connectMe.com",
+		"https://github.com",
+		"https://fb.com",
+	}
+
+	fmt.Println("Function execution without goo routine:")
+	for _, site := range websiteList {
+		getStatusCodeWithoutGoRoutine(site)
+	}
+
+	fmt.Println("\nFunction execution with Goroutine")
+	for _, site := range websiteList {
+		go getStatusCodeWithGoRoutine(site)
+
+		waitGroup.Add(1)
+	}
+
+	waitGroup.Wait()
 }
 
-func greeter(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(3 * time.Millisecond)
-		fmt.Println(s)
+// func greeter(s string) {
+// 	for i := 0; i < 5; i++ {
+// 		time.Sleep(3 * time.Millisecond)
+// 		fmt.Println(s)
+// 	}
+// }
+
+func getStatusCodeWithGoRoutine(endpoint string) {
+
+	defer waitGroup.Done()
+
+	result, err := http.Get(endpoint)
+
+	if err != nil {
+		panic(err)
 	}
+
+	fmt.Printf("%d status code for the endpoint: %s\n", result.StatusCode, endpoint)
+}
+
+func getStatusCodeWithoutGoRoutine(endpoint string) {
+	result, err := http.Get(endpoint)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%d status code for the endpoint: %s\n", result.StatusCode, endpoint)
 }
